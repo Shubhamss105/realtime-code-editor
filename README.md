abfa7ddc5c8415c10f4812603b62ef53895dd8d4
Shubhamss105 <shubhamsingh.ss.1407@gmail.com>
2024-02-10 19:19:29 +0530

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-### Demo
+This project is a real-time code editor application built with React.js on the frontend, Node.js on the backend, and Socket.io for enabling real-time communication between the client and server.
 
-https://code-sync.codersgyan.com/
+Technologies Used
+React.js: A JavaScript library for building user interfaces, used for the frontend development.
+Node.js: A JavaScript runtime built on Chrome's V8 JavaScript engine, used for the backend development.
+Socket.io: A JavaScript library for real-time web applications, used for enabling real-time communication between client and server.
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -67,7 +67,7 @@ const Home = () => {
                         If you don't have an invite then create &nbsp;
                         <a
                             onClick={createNewRoom}
-                            href=""
+                            href=" "
                             className="createNewBtn"
                         >
                             new room
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="https://github.com/Shubhamss105">Shubham Singh</a>
                 </h4>
             </footer>
         </div>
